a7ae6993dfb84fcfe3466b0b1485ae70900f26d4
rritsu <andrea_legaspi@dlsu.edu.ph>
2024-05-17 23:10:24 +0800

    For Assignment 2 uwu by Andrea Maxene Legaspi

--- gdphysx-code/P6/MyVector.cpp
+++ gdphysx-code/P6/MyVector.cpp
@@ -1,1 +1,57 @@
 #include "MyVector.h"
+using namespace P6;
+
+//MAGNITUDE
+float MyVector::getMagnitude() {
+	return sqrt(pow(this->x, 2) + pow(this->y, 2));
+}
+
+//DIRECTION
+MyVector MyVector::getDirection() {
+	float d = 1/this->getMagnitude();
+	MyVector direction(d * this->x, d * this->y, d * this->z);
+	return direction;
+}
+
+//ADDITION
+void MyVector::operator+= (const MyVector v) {
+	this->x += v.x;
+	this->y += v.y;
+	this->z += v.z;
+}
+
+//SUBTRACTION
+void MyVector::operator-= (const MyVector v) {
+	this->x -= v.x;
+	this->y -= v.y;
+	this->z -= v.z;
+}
+
+//SCALAR MULTIPLICATION
+void MyVector::scalarMultiplication (const float v) {
+	this->x *= v;
+	this->y *= v;
+	this->z *= v;
+}
+
+//COMPONENT PRODUCT
+void MyVector::operator*= (const MyVector v) {
+	this->x *= v.x;
+	this->y *= v.y;
+	this->z *= v.z;
+}
+
+//SCALAR PRODUCT
+float MyVector::scalarProduct (const MyVector v) {
+	return this->x * v.x + this->y * v.y + this->z * v.z;
+}
+
+//VECTOR PRODUCT
+MyVector MyVector::getCrossProduct (const MyVector v) {
+	float x = this->y * v.z - this->z * v.y;
+	float y = this->z * v.x - this->x * v.z;
+	float z = this->x * v.y - this->y * v.x;
+	MyVector crossProduct(x, y, z);
+
+	return crossProduct;
+}
--- gdphysx-code/P6/MyVector.h
+++ gdphysx-code/P6/MyVector.h
@@ -9,31 +9,21 @@ namespace P6 {
 	{
 		public:
 			float x, y, z;
-
+			
 			MyVector() : x(0), y(0), z(0) {}
 			MyVector(const float _x, const float _y, const float _z) : x(_x), y(_y), z(_z) {}
 
-			explicit operator glm::vec3() const { return glm::vec3(x, y, z); }
+			explicit operator glm::vec3() const { return glm::vec3(x, y, z); }  //for typecast
 
-			MyVector operator+ (const MyVector v) {
-				return MyVector(this->x + v.x, 
-								this->y + v.y,
-								this->z + v.z);
-			}
+			float getMagnitude();
+			MyVector getDirection();
+			void operator+= (const MyVector v);
+			void operator-= (const MyVector v);
+			void scalarMultiplication(const float v);
+			void operator*=(const MyVector v);
+			float scalarProduct(const MyVector v);
+			MyVector getCrossProduct(const MyVector v);
 
-			void operator+= (const MyVector v) {
-				this->x += v.x;
-				this->y += v.y;
-				this->z += v.z;
-			}
-
-			MyVector Add(MyVector b) {
-				return MyVector(
-					this->x + b.x,
-					this->y + b.y,
-					this->z + b.z
-				);
-			}
 	};
 }
 
